2fdcc36df2ef5590b9eb0de11f20049ec5c0b906
silent <soichiro260@gmail.com>
2025-05-18 03:02:53 +0900

    pyw:windows用イベントハンドラーとしてマウスホイールで画像偏移を追加!

--- gazowa.bat
+++ gazowa.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:\Users\soich\AppData\Local\Programs\Python\Python313\pythonw.exe" "C:\Users\soich\OneDrive\ÉhÉLÉÖÉÅÉìÉg\gazowa2.pyw" "%1"
+start "" "C:\Users\silent\AppData\Local\Programs\Python\Python313\pythonw.exe" "C:\Users\silent\Documents\gazowa2\gazowa2.pyw" "%1"
